d100bfbc6b521b9f9c9a16509e6636c703af60e8
Adam Bieszk <adambieszk6@gmail.com>
2025-07-11 21:28:45 +0200

    refactor: moved respriting Isaac to seperated function
    renamed global variable for mod
    
    simplify noSpace method

--- main.lua
+++ main.lua
@@ -1,70 +1,16 @@
-local MOD = RegisterMod("Better Unique Isaac Fight", 1)
+local betterUniqueIsaacFight = RegisterMod("Better Unique Isaac Fight", 1)
 local spawnedCorpse = {}
 local SaveState = {}
 local json = require("json")
 local modconfig = require("variantsconfig")
 modconfig.AddModConfigMenu()
 
-function MOD:OnFrame()
-	local playertype = Isaac.GetPlayer(0):GetPlayerType()
+function betterUniqueIsaacFight:OnFrame()
+	local player = Isaac.GetPlayer(0)
 	for _, entity in pairs(Isaac.GetRoomEntities()) do
 		local sprite = entity:GetSprite()
 		if entity.Type == 102 and entity.Variant == 0 then
-			if playertype == PlayerType.PLAYER_MAGDALENE or playertype == PlayerType.PLAYER_MAGDALENE_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Magdalene/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_CAIN or playertype == PlayerType.PLAYER_CAIN_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Cain/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_JUDAS or playertype == PlayerType.PLAYER_JUDAS_B or playertype == PlayerType.PLAYER_BLACKJUDAS then
-				sprite:ReplaceSpritesheet(0, MOD:returnJudasVariantPath())
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_BLUEBABY or playertype == PlayerType.PLAYER_BLUEBABY_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/BlueBaby/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_EVE or playertype == PlayerType.PLAYER_EVE_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Eve/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_SAMSON or playertype == PlayerType.PLAYER_SAMSON_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Samson/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_AZAZEL or playertype == PlayerType.PLAYER_AZAZEL_B then
-				sprite:ReplaceSpritesheet(0, MOD:returnAzazelVariantPath())
-				sprite:LoadGraphics()
-			-- or PlayerType.PLAYER_LAZARUS2
-			elseif playertype == PlayerType.PLAYER_LAZARUS or playertype == PlayerType.PLAYER_LAZARUS_B or playertype == PlayerType.PLAYER_LAZARUS2_B then
-				-- Lazarus Risen is cut off because for some reason he replaces other characters above him
-				sprite:ReplaceSpritesheet(0, MOD:returnLazarusVariantPath())
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_EDEN or playertype == PlayerType.PLAYER_EDEN_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Eden/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_THELOST or playertype == PlayerType.PLAYER_THELOST_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/TheLost/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_LILITH or playertype == PlayerType.PLAYER_LILITH_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Lilith/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_KEEPER or playertype == PlayerType.PLAYER_KEEPER_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Keeper/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_APOLLYON or playertype == PlayerType.PLAYER_APOLLYON_B then
-				print(MOD:returnApollyonVariantPath())
-				sprite:ReplaceSpritesheet(0, MOD:returnApollyonVariantPath())
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_THEFORGOTTEN or playertype == PlayerType.PLAYER_THESOUL or playertype == PlayerType.PLAYER_THEFORGOTTEN_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/TheForgotten/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_BETHANY or playertype == PlayerType.PLAYER_BETHANY_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Bethany/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			elseif playertype == PlayerType.PLAYER_JACOB or playertype == PlayerType.PLAYER_JACOB_B or playertype == PlayerType.PLAYER_JACOB2_B then
-				sprite:ReplaceSpritesheet(0, "gfx/dudes/Jacob/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			else
-				sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/boss_075_isaac.png")
-				sprite:LoadGraphics()
-			end
+			betterUniqueIsaacFight:replaceIsaac(player,sprite)
 		end
 		if entity.Type == EntityType.ENTITY_BABY and entity.Variant == 1 and #Isaac.FindByType(102, 0, -1, false, true) >= 1 then
 			if (playertype == PlayerType.PLAYER_CAIN or playertype == PlayerType.PLAYER_CAIN_B) and modconfig.replaceGuardianAngelsForCain then
@@ -81,11 +27,22 @@ function MOD:OnFrame()
 	end
 end
 
-function MOD:initVars(npc)
+--- @param player EntityPlayer
+--- @param sprite Sprite
+--- @return nil
+function betterUniqueIsaacFight:replaceIsaac(player, sprite)
+	local spritePath = "gfx/dudes/" .. player:GetName() .. "/boss_075_isaac.png"
+	sprite:ReplaceSpritesheet(0, spritePath)
+	sprite:LoadGraphics()
+end
+
+--- @param npc EntityType
+function betterUniqueIsaacFight:initVars(npc)
 	spawnedCorpse = {}
 end
 
-function MOD:YO(npc)
+--- @param npc EntityType
+function betterUniqueIsaacFight:YO(npc)
 	local playertype = Isaac.GetPlayer(0):GetPlayerType()
 	if not npc or npc.Variant ~= 0 then
 		return
@@ -96,13 +53,13 @@ function MOD:YO(npc)
 	if sprite:IsFinished("2Evolve") and not spawnedCorpse[npc.Index] then
 		if playertype == 10 or playertype == 16 or playertype == 17 or playertype == 35 or playertype == 31 then
 			local newCorpse = Isaac.Spawn(102, 0, 1, npc.Position, Vector(0.0, 0.0), npc)
-			MOD:animateCorpse(newCorpse)
+			betterUniqueIsaacFight:animateCorpse(newCorpse)
 			spawnedCorpse[npc.Index] = newCorpse
 		end
 	end
 end
 
-function MOD:animateCorpse(corpse)
+function betterUniqueIsaacFight:animateCorpse(corpse)
 	corpse:AddEntityFlags(EntityFlag.FLAG_FRIENDLY)
 	corpse:AddEntityFlags(EntityFlag.FLAG_NO_TARGET)
 	corpse.EntityCollisionClass = EntityCollisionClass.ENTCOLL_NONE
@@ -111,7 +68,7 @@ function MOD:animateCorpse(corpse)
 	sprite:Play("2EvolveDead")
 end
 
-function MOD:killMyCorpse(npc)
+function betterUniqueIsaacFight:killMyCorpse(npc)
 	local playertype = Isaac.GetPlayer(0):GetPlayerType()
 	if not npc or npc.Variant ~= 0 then
 		return
@@ -129,7 +86,7 @@ function MOD:killMyCorpse(npc)
 end
 
 -- save config to data directory
-function MOD:saveModData()
+function betterUniqueIsaacFight:saveModData()
 	SaveState.selectedApollyonVariant = {}
 	SaveState.selectedAzazelVariant = {}
 	SaveState.selectedJudasVariant = {}
@@ -166,13 +123,13 @@ function MOD:saveModData()
 		SaveState.replaceGuardianAngelsForJacob[tostring(i)] = modconfig[i]
 	end
 
-	mod:SaveData(json.encode(SaveState))
+	betterUniqueIsaacFight:SaveData(json.encode(SaveState))
 end
 
 -- Load data from file
-function MOD:LoadModData()
-	if MOD:HasData() then
-		SaveState = json.decode(mod:LoadData())
+function betterUniqueIsaacFight:LoadModData()
+	if betterUniqueIsaacFight:HasData() then
+		SaveState = json.decode(betterUniqueIsaacFight:LoadData())
 
 		for i,_ in pairs(SaveState.selectedApollyonVariant) do
 			modconfig[tostring[i]] = SaveState.selectedApollyonVariant[i]
@@ -206,31 +163,29 @@ end
 
 
 --fixes for invisible characters
-function MOD:returnJudasVariantPath()
+function betterUniqueIsaacFight:returnJudasVariantPath()
 	return "gfx/dudes/Judas/" .. noSpace(modconfig.getSelectedJudasVariant())  .. "/boss_075_isaac.png"
 end
 
-function MOD:returnAzazelVariantPath()
+function betterUniqueIsaacFight:returnAzazelVariantPath()
 	return "gfx/dudes/Azazel/" .. noSpace(modconfig.getSelectedAzazelVariant())  .. "/boss_075_isaac.png"
 end
 
-function MOD:returnLazarusVariantPath()
+function betterUniqueIsaacFight:returnLazarusVariantPath()
 	return "gfx/dudes/Lazarus/" .. noSpace(modconfig.getSelectedLazarusVariant())  .. "/boss_075_isaac.png"
 end
 
-function MOD:returnApollyonVariantPath()
+function betterUniqueIsaacFight:returnApollyonVariantPath()
 	return "gfx/dudes/Apollyon/" .. noSpace(modconfig.getSelectedApollyonVariant())  .. "/boss_075_isaac.png"
 end
 
 function noSpace(str)
-
-	local normalisedString = string.gsub(str, "%s+", "")
-	return normalisedString
+	return string.gsub(str, "%s+", "")
 end
 
-MOD:AddCallback(ModCallbacks.MC_POST_NPC_INIT, MOD.initVars, EntityType.ENTITY_ISAAC)
-MOD:AddCallback(ModCallbacks.MC_NPC_UPDATE, MOD.YO, EntityType.ENTITY_ISAAC)
-MOD:AddCallback(ModCallbacks.MC_POST_UPDATE, MOD.OnFrame)
-MOD:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, MOD.killMyCorpse, EntityType.ENTITY_ISAAC)
-MOD:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, MOD.saveModData)
-MOD:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, MOD.LoadModData)
+betterUniqueIsaacFight:AddCallback(ModCallbacks.MC_POST_NPC_INIT, betterUniqueIsaacFight.initVars, EntityType.ENTITY_ISAAC)
+betterUniqueIsaacFight:AddCallback(ModCallbacks.MC_NPC_UPDATE, betterUniqueIsaacFight.YO, EntityType.ENTITY_ISAAC)
+betterUniqueIsaacFight:AddCallback(ModCallbacks.MC_POST_UPDATE, betterUniqueIsaacFight.OnFrame)
+betterUniqueIsaacFight:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, betterUniqueIsaacFight.killMyCorpse, EntityType.ENTITY_ISAAC)
+betterUniqueIsaacFight:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, betterUniqueIsaacFight.saveModData)
+betterUniqueIsaacFight:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, betterUniqueIsaacFight.LoadModData)
